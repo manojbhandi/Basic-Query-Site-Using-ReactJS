0690b8862caff71e15a570eca6c004c96d23229d
manojbhandi <bmanojkumar5@gmail.com>
2022-07-14 20:20:12 +0530

    export import assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import SubmitButton from "./SubmitButton";
+import SubHeading from "./SubHeading";
+import InputQuery from "./InputQuery";
+import Heading from "./Heading";
+
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery />
+      <SubHeading />
+      <SubmitButton />
     </div>
   )
 }
+export default App;
 
 
